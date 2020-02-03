ec20c9658bd9e90544fb80b383df1cb8916f0884
Krzysiek Kita <aylszen@gmail.com>
2020-02-03 20:42:41 +0100

    Loading data function added

--- main.py
+++ main.py
@@ -1,51 +1,30 @@
 import numpy as np
 
-A = [[3, 2, 2, 1]]
-print(A)
-new_array_2 = np.vstack([A , [[2, 3, 0, 7]]])
-print(new_array_2)
-new_array = np.vstack([new_array_2, [[-1, 2, 2,-3]]])
-print(new_array)
+
+def load_data(path_to_file):
+    matrices = {}
+    with open(path_to_file) as file:
+        all_lines = file.readlines()
+        all_relevant_lines = all_lines[1::]
+        for line in all_relevant_lines:
+            line = line.replace('\n', '').split(',')
+            name = line[0]
+            numbers = line[1:]
+            for i in range(len(numbers)):
+                numbers[i] = float(numbers[i])
+            name = name.split()[0]
+            if name in matrices:
+                matrices[name] = np.append(matrices.get(name), [numbers], axis=0)
+            else:
+                matrices[name] = np.array([numbers])
+        file.close()
+    return matrices
 
 
-def matrix_mean(matrix, axis_value):
-    return np.mean(matrix, axis=axis_value)
+def main():
+    matrices = load_data("Maple_Oak.txt")
+    print(matrices["Acer"])
 
 
-matrix_mean2 = matrix_mean(new_array, 1)
-print (matrix_mean2)
-
-def add_row_to_array(old_array, row_data):
-    new_array = np.vstack([old_array, [row_data]])
-    return new_array
-
-
-def euclidean_distance(matrix, mean_matrix):
-    print(mean_matrix)
-    for element in mean_matrix:
-        print(element)
-    i = 0
-    k = 0
-    for column in matrix.T:
-        for element in column:
-            k += np.sum((element - mean_matrix[i]) ** 2)
-            print("Element")
-            print(element)
-            print ("mean_matrix")
-            mean_matrix[i]
-            print(np.sum((element - mean_matrix[i]) ** 2))
-            i+=1
-        i=0
-        print("Weszlo")
-        #np.sqrt(np.sum((v - u) ** 2))
-    print(np.sqrt(k))
-
-print("###")
-print(euclidean_distance(new_array, matrix_mean2))
-
-#temp_new_array = add_row_to_array(new_array, [9,10,2])
-#print(temp_new_array)
-
-
-def fisher_algorithm(data):
-    print("Hello")
+if __name__ == "__main__":
+    main()
