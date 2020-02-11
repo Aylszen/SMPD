c9a48bd2e10c0eff44198b75183ca87ff581d9fc
Krzysiek Kita <aylszen@gmail.com>
2020-02-11 20:22:44 +0100

    Small updates

--- src/classification.py
+++ src/classification.py
@@ -24,6 +24,21 @@ def classify_one_best_characteristic(distances_matrix_a, distances_matrix_b, siz
     return indexes_list
 
 
+def classify_k_best_characteristics(distances_matrix_a, distances_matrix_b, size, k):
+    indexes_list = []
+    for i in range(0, size):
+        if distances_matrix_a[i][0] > distances_matrix_b[i][0]:
+            indexes_list.append(0)
+        elif distances_matrix_b[i][0] > distances_matrix_a[i][0]:
+            indexes_list.append(1)
+        else:
+            indexes_list.append(2)
+
+    print(len(indexes_list))
+    print(indexes_list)
+    return indexes_list
+
+
 def nn_classification(training_set, matrix_after_sel_alg):
     distances_matrix_a = []
     distances_matrix_b = []
@@ -46,27 +61,20 @@ def calculate_efficiency(classification_result, matrices_all, training_set):
     training_set_b_t = training_set[1].transpose()
 
     for i in range(0, len(training_set_a_t)):
-        if classification_result[i] == 0:
+        if classification_result[i] != 1:
             if training_set_a_t[i] in matrices_all[0]:
-                print("Weszlo1")
                 efficiency = efficiency + 1
-        elif classification_result[i] == 1:
-            if training_set_a_t[i] in matrices_all[1]:
-                print("Weszlo2")
-                #efficiency = efficiency + 1
-        else:
-            efficiency +=1
+        elif classification_result[i + len(training_set_a_t)] == 2:
+            efficiency = efficiency + 1
 
     for i in range(0, len(training_set_b_t)):
-        if classification_result[i + len(training_set_a_t)] == 0:
-            if training_set_b_t[i] in matrices_all[0]:
-                print("Weszlo3")
-        elif classification_result[i + len(training_set_a_t)] == 1:
+        if classification_result[i + len(training_set_a_t)] == 1:
             if training_set_b_t[i] in matrices_all[1]:
                 print("Weszlo4")
                 efficiency = efficiency + 1
-        else:
-            efficiency +=1
+        elif classification_result[i + len(training_set_a_t)] == 2:
+            efficiency = efficiency + 1
+
     training_set_len = len(training_set[0].transpose()) + len(training_set[1].transpose())
     print(training_set_len)
     print("Len training set: ", len(training_set[0]))
--- src/main.py
+++ src/main.py
@@ -57,8 +57,6 @@ def create_matrices_based_on_sel_alg(the_best_characteristics, learning_set):
             new_matrix_a = np.append(new_matrix_a, [learning_set[0][characteristic]], axis=0)
             new_matrix_b = np.append(new_matrix_b, [learning_set[1][characteristic]], axis=0)
 
-    print("Dlugocs tab_a: ", len(new_matrix_a))
-    print("Dlugocs tab_b: ", len(new_matrix_b))
 
     new_matrix = []
     new_matrix.append(new_matrix_a)
@@ -69,7 +67,7 @@ def create_matrices_based_on_sel_alg(the_best_characteristics, learning_set):
 def main():
     path_to_file = "../Maple_Oak.txt"
     percentage = 20
-    number_of_characteristics = 2
+    number_of_characteristics = 3
     selection = "SFS"  # SFS or F
     classification = "NN"  # NN kNN
     matrices = load_data(path_to_file)
